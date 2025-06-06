4926b53bac8dd203257c40541cfcc62dc4c40d77
Sergey Bogomolov <bogomolovs693@gmail.com>
2025-06-06 19:33:20 +0300

    update next config

--- client/next.config.ts
+++ client/next.config.ts
@@ -11,22 +11,8 @@ const nextConfig: NextConfig = {
         search: '',
       },
       {
-        protocol: 'http',
-        hostname: '91.219.189.214',
-        port: '1337',
-        pathname: '/uploads/**',
-        search: '',
-      },
-      {
-        protocol: 'http',
-        hostname: 'museum.grekas.ru',
-        port: '1337',
-        pathname: '/uploads/**',
-        search: '',
-      },
-      {
         protocol: 'https',
-        hostname: 'museum.grekas.ru',
+        hostname: 'admin.demidov-museum.ru',
         port: '1337',
         pathname: '/uploads/**',
         search: '',
